1a4693b2662461ac6b0ef069aff03e918583da60
Pawan Choudhari <choudhari.pawan2595@gmail.com>
2024-01-31 20:20:31 +0530

    Initial layout added, updated packages

---
+++ src/components/SideNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { List, ListItemButton, ListItemIcon, ListItemText } from '@mui/material';
+import { Link, useLocation } from 'react-router-dom';
+import SettingsSuggestRoundedIcon from '@mui/icons-material/SettingsSuggestRounded';
+import DashboardRoundedIcon from '@mui/icons-material/DashboardRounded';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+
+function SideNav() {
+    const location = useLocation();
+  return (
+    <List>
+      <ListItemButton component={Link} to="/" selected={location.pathname === '/'}>
+      <ListItemIcon sx={{ minWidth: 'unset', marginRight: '10px' }} >
+          <HomeRoundedIcon  color='secondary' />
+        </ListItemIcon>
+        <ListItemText primary="Home" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/dashboard" selected={location.pathname === '/dashboard'}>
+      <ListItemIcon sx={{ minWidth: 'unset', marginRight: '10px' }}>
+          <DashboardRoundedIcon color='secondary' />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItemButton>
+      <ListItemButton component={Link} to="/settings" selected={location.pathname === '/settings'} icon>
+        <ListItemIcon sx={{ minWidth: 'unset', marginRight: '10px' }}>
+          <SettingsSuggestRoundedIcon color='secondary' />
+        </ListItemIcon>
+        <ListItemText primary="Settings" />
+      </ListItemButton>
+    </List>
+  );
+}
+
+export default SideNav;
