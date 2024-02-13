e99ad713064a22fb5ea4552ace28f6b6dca268a7
Pawan Choudhari <choudhari.pawan2595@gmail.com>
2024-02-13 15:15:50 +0530

    Update Dashboard.js

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,15 +2,17 @@ import React from 'react';
 import CardComponent from './CardComponent';
 import {CONSOLE_DATA, DEPLOYMENT_OVERVIEW_OPTIONS, DEPLOYMENT_OVERVIEW_OPTIONS_2} from '../MOCK_DATA';
 import Piechart from './Piechart';
-import {useTheme} from '@emotion/react';
+import useUserStore from '../stores/userStore';
 
 const Dashboard = () => {
-  const theme = useTheme()
+  const { isDarkTheme } = useUserStore()
+
   return (
     <>
       <h3>Dashboard</h3>
       <CardComponent consoleData={CONSOLE_DATA} />
-      <Piechart options1={theme.palette.mode === 'dark' ? DEPLOYMENT_OVERVIEW_OPTIONS : DEPLOYMENT_OVERVIEW_OPTIONS_2}/>
+      {isDarkTheme && <Piechart options1={DEPLOYMENT_OVERVIEW_OPTIONS} /> }
+      {!isDarkTheme && <Piechart options1={DEPLOYMENT_OVERVIEW_OPTIONS_2} /> }
     </>
   );
 };
